471208a127f807d862c01482fb11392ac39bd99d
luyenhaidangit <luyenhaidangit@gmail.com>
2022-12-30 18:50:54 +0700

    ES6 Classes

---
+++ JavaScript ES6/Practice 3/main.js
@@ -0,0 +1,17 @@
+//Object
+let person = {
+    name: "luyenhaidangit",
+    age: 21,
+}
+
+let person1 = {
+    "name": "luyenhaidangit",
+    "age": 21,
+}
+
+console.log("Check person:",person);
+console.log("Check person:",person1);
+
+//Accessing Object Properties
+console.log(person.name);
+console.log(person["name"]);
