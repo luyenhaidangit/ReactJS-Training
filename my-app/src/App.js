59c747c635ee71f835c8587afba4049c2fc52930
luyenhaidangit <luyenhaidangit@gmail.com>
2022-12-31 11:14:06 +0700

    State React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-import MyComponents from './components/MyComponents.js'
+import logo from "./logo.svg";
+import "./App.css";
+import MyComponents from "./components/MyComponents.js";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <MyComponents>
-            
-          </MyComponents>
-          <br>
-          </br>Hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <MyComponents></MyComponents>
+            <br></br>Hello world!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           <MyComponents>
+
+//           </MyComponents>
+//           <br>
+//           </br>Hello world!
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- my-app/src/components/MyComponents.js
+++ my-app/src/components/MyComponents.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 class MyComponents extends React.Component{
+    state = {
+        name: "luyenhaidangit",
+        andress: "hung yen",
+        age: 21,
+    }
+
     render(){
         return(
-            <div>Luyen Hai Dang</div>
+            <div>
+                {this.state.name}
+                <br></br>
+                {this.state.andress}
+                <br></br>
+                {this.state.age}
+            </div>
         );
     }
 }
