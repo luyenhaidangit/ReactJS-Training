5ae2cc5d242df6dc12c6eb3ee862df542624d020
luyenhaidangit <luyenhaidangit@gmail.com>
2023-01-01 00:34:37 +0700

    Conditional Output

--- my-app/src/components/DisplayInfo.js
+++ my-app/src/components/DisplayInfo.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React from "react";
 
-class DisplayInfo extends React.Component{
-   
-    render(){
-        const {users} = this.props;
-        console.log(users);
-        return(
-           <div>
-            {
-                users.map((user)=>{
-                    return(
-                        <div key={user.id}>
-                            ID:{user.id}--Name:{user.name}--Age:{user.age}
-                        </div>
-                    );
-                })
-            }
-           </div>
-        );
+class DisplayInfo extends React.Component {
+    state = {
+        statusUsers : true,
     }
+
+    hideUser = ()=>{
+        this.setState({
+            statusUsers : !this.state.statusUsers,
+        });
+    }
+
+  render() {
+    const { users } = this.props;
+    console.log(users);
+    return (
+      <div>
+        <div>
+            <span onClick={()=>{this.hideUser()}}>
+                {this.state.statusUsers === true ? "hide users" : "show users"}
+            </span>
+        </div>
+        {
+            this.state.statusUsers && <div>
+            {users.map((user) => {
+              return (
+                <div key={user.id} className={user.age > 22 ? "red" : "blue"}>
+                  ID:{user.id}--Name:{user.name}--Age:{user.age}
+                </div>
+              );
+            })}
+          </div>
+        }
+        
+      </div>
+    );
+  }
 }
 
 export default DisplayInfo;
