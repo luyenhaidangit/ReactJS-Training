1a2be606c00491373aca1535fc6cd25ed343804c
luyenhaidangit <luyenhaidangit@gmail.com>
2023-01-02 07:42:06 +0700

    StateFulStateless Component React

--- my-app/src/components/DisplayInfo.js
+++ my-app/src/components/DisplayInfo.js
@@ -1,43 +1,69 @@
 import React from "react";
 import './DisplayInfo.scss'
 
-class DisplayInfo extends React.Component {
-    state = {
-        statusUsers : true,
-    }
+// class DisplayInfo extends React.Component {
+//     // state = {
+//     //     statusUsers : true,
+//     // }
 
-    hideUser = ()=>{
-        this.setState({
-            statusUsers : !this.state.statusUsers,
-        });
-    }
+//     // hideUser = ()=>{
+//     //     this.setState({
+//     //         statusUsers : !this.state.statusUsers,
+//     //     });
+//     // }
 
-  render() {
-    const { users } = this.props;
-    console.log(this.props);
-    return (
-      <div className="display-info">
-        <div>
-            <span onClick={()=>{this.hideUser()}}>
-                {this.state.statusUsers === true ? "hide users" : "show users"}
-            </span>
-        </div>
-        {
-            this.state.statusUsers && <div>
-            {users.map((user) => {
-              return (
-                <div key={user.id} className={user.age > 22 ? "red" : "white"} style={{fontSize:'16px',paddingTop:'4px',}}>
-                  ID:{user.id}--Name:{user.name}--Age:{user.age} 
-                  <button onClick={()=>this.props.HandleDeleteUser(user.id)}>Delete</button>
-                </div>
-              );
-            })}
-          </div>
-        }
+//   render() {
+//     const { users } = this.props;
+//     console.log(this.props);
+//     return (
+//       <div className="display-info">
+//         {/* <div>
+//             <span onClick={()=>{this.hideUser()}}>
+//                 {this.state.statusUsers === true ? "hide users" : "show users"}
+//             </span>
+//         </div> */}
+//         {
+//             true && <div>
+//             {users.map((user) => {
+//               return (
+//                 <div key={user.id} className={user.age > 22 ? "red" : "white"} style={{fontSize:'16px',paddingTop:'4px',}}>
+//                   ID:{user.id}--Name:{user.name}--Age:{user.age} 
+//                   <button onClick={()=>this.props.HandleDeleteUser(user.id)}>Delete</button>
+//                 </div>
+//               );
+//             })}
+//           </div>
+//         }
         
-      </div>
-    );
-  }
+//       </div>
+//     );
+//   }
+// }
+
+const DisplayInfo = (props)=>{
+  const { users } = props;
+  return (
+    <div className="display-info">
+      {/* <div>
+          <span onClick={()=>{this.hideUser()}}>
+              {this.state.statusUsers === true ? "hide users" : "show users"}
+          </span>
+      </div> */}
+      {
+          true && <div>
+          {users.map((user) => {
+            return (
+              <div key={user.id} className={user.age > 22 ? "red" : "white"} style={{fontSize:'16px',paddingTop:'4px',}}>
+                ID:{user.id}--Name:{user.name}--Age:{user.age} 
+                <button onClick={()=>this.props.HandleDeleteUser(user.id)}>Delete</button>
+              </div>
+            );
+          })}
+        </div>
+      }
+      
+    </div>
+  );
 }
 
 export default DisplayInfo;
