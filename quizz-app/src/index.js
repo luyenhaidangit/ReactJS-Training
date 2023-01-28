a168ff61e3e17c2f8d281fd773b4a5c9fd44ed72
luyenhaidangit <106638147+luyenhaidangit@users.noreply.github.com>
2023-01-28 12:00:14 +0700

    Config Redux

--- quizz-app/src/index.js
+++ quizz-app/src/index.js
@@ -5,17 +5,23 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from "react-router-dom";
 import Layout from './Layout';
+import { Provider } from "react-redux";
+import store from './redux/store';
+
 
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <Layout></Layout>
-  </BrowserRouter>
-  // </React.StrictMode>
+  <Provider store={store}>
+    {/* <React.StrictMode> */}
+    <BrowserRouter>
+      <Layout></Layout>
+    </BrowserRouter>
+    {/* </React.StrictMode> */}
+  </Provider >
+
 );
 
 // If you want to start measuring performance in your app, pass a function
