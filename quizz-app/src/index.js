d2cfb4924a6cc2feef86436ccd1adf1cfd701303
luyenhaidangit <106638147+luyenhaidangit@users.noreply.github.com>
2023-01-29 20:34:41 +0700

    Save Data User In Redux

--- quizz-app/src/index.js
+++ quizz-app/src/index.js
@@ -6,8 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from "react-router-dom";
 import Layout from './Layout';
 import { Provider } from "react-redux";
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 import "nprogress/nprogress.css"
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 
@@ -16,11 +17,14 @@ import "nprogress/nprogress.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    {/* <React.StrictMode> */}
-    <BrowserRouter>
-      <Layout></Layout>
-    </BrowserRouter>
-    {/* </React.StrictMode> */}
+    <PersistGate loading={null} persistor={persistor}>
+      {/* <React.StrictMode> */}
+      <BrowserRouter>
+        <Layout></Layout>
+      </BrowserRouter>
+      {/* </React.StrictMode> */}
+    </PersistGate>
+
   </Provider >
 
 );
--- quizz-app/src/redux/store.js
+++ quizz-app/src/redux/store.js
@@ -2,10 +2,21 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducer/rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key: "root",
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = createStore(
-    rootReducer,
+    persistedReducer,
     composeWithDevTools(applyMiddleware(thunk))
 );
 
-export default store;
+let persistor = persistStore(store);
+
+export { store, persistor };
