9b8f5ee49664013ac9f6a85774d9972b7d299795
luyenhaidangit <106638147+luyenhaidangit@users.noreply.github.com>
2023-02-01 22:44:32 +0700

    Design Base Layout Quizz

--- quizz-app/src/components/user/DetailQuizz.js
+++ quizz-app/src/components/user/DetailQuizz.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "./DetailQuizz.scss";
 
 const DetailQuizz = (props) => {
     const params = useParams();
@@ -18,9 +19,37 @@ const DetailQuizz = (props) => {
     }
 
     return (
-        <>
-            Trang detail quizz
-        </>
+        <div className="quizz-container d-flex">
+            <div className="left-content">
+                <div className="title">
+                    Quizz {quizzId}: Day la quizz
+                </div>
+                <div className="question">
+                    <div className="question-title">
+                        Question 1: Day la cau hoi
+                    </div>
+                    <div className="question-body">
+                        <div className="a-child">
+                            A. a child
+                        </div>
+                        <div className="b-child">
+                            B. b child
+                        </div>
+                        <div className="c-child">
+                            C. c child
+                        </div>
+                    </div>
+                    <div className="question-footer d-flex justify-content-center">
+                        <button className="btn btn-secondary">Previous</button>
+                        <button className="btn btn-primary">Next</button>
+                    </div>
+                </div>
+
+            </div>
+            <div className="right-content">
+                count down
+            </div>
+        </div>
     );
 }
 
