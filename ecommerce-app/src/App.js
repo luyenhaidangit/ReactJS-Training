b9ba396d40aa242a250bd0c8cb3f0b808ed302c0
luyenhaidangit <106638147+luyenhaidangit@users.noreply.github.com>
2023-02-03 22:05:03 +0700

    Base Project React

--- ecommerce-app/src/App.js
+++ ecommerce-app/src/App.js
@@ -1,10 +1,31 @@
+// Lib
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// Client
+import Layout from './layouts/client/Layout';
+import Home from './pages/Home/Home';
+import Product from './pages/Product/Product';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+// Style
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+
 
 function App() {
   return (
     <>
-      This Is App
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='product' element={<Product />} />
+            <Route path='contact' element={<Contact />} />
+            <Route path='about' element={<About />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
